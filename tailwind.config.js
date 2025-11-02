57247f95d90684f5a1e085ab1e0485dce7e32ac8
Zyprush18 <zyprush9@gmail.com>
2025-11-03 04:03:36 +0800

    fix (layout): home,about,focus and contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,9 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                nunito: ['"Nunito Sans"', 'sans-serif'],
+                nunito: ['"Nunito Sans"', "sans-serif"],
             },
         },
     },
     plugins: [],
-}
+};
